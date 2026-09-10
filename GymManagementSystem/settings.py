caddbff40d5d619eab8d2014c9c5322285b148d7
Buildwithaayush <iaayush.dev@gmail.com>
2026-09-10 20:54:39 +0545

    db changes from sqlite to postgresql

--- GymManagementSystem/settings.py
+++ GymManagementSystem/settings.py
@@ -101,16 +101,17 @@ WSGI_APPLICATION = 'GymManagementSystem.wsgi.application'
 #         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
 #         conn_max_age=600
 #     )
-
 # }
 DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gym_management',
+        'USER': 'admin',
+        'PASSWORD': 'aayushshah.com',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
